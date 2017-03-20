633507422dab5cc4b93775695791eb351f75880e
89leon <89leon@gmail.com>
2017-03-20 20:05:10 +0200

    capuring added

--- app/app.py
+++ app/app.py
@@ -1,22 +1,15 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask , request , render_template
 
 app = Flask(__name__)
 
-
-@app.route("/hello")
-def hello():
-    return "Hello, World!"
-
-@app.route("/", methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        value_one = int(request.form['first'])
-        value_two = int(request.form['second'])
-        total = value_one + value_two
-        data = {"total": str(total)}
-        return jsonify(data)
-    return render_template('index.html')
-
-
+@app.route("/")
+def index(): 
+		return render_template('index.html')
+		
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
+
+
+
+
+
