8d95c38a382c92fb667aeb638946b64fbf4675a3
89leon <89leon@gmail.com>
2017-03-20 18:54:27 +0200

    am

--- app/app.py
+++ app/app.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-	return "Hello ,World!"
+
+@app.route("/hello")
+def hello():
+    return "Hello, World!"
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        value_one = int(request.form['first'])
+        value_two = int(request.form['second'])
+        total = value_one + value_two
+        data = {"total": str(total)}
+        return jsonify(data)
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
-
-
-
-
-
+    app.run(debug=True)
